e8f600da168106133863e0c4c90992d5023338b3
harish <muthumuruganharish272003@gmail.com>
2026-01-12 18:22:11 +0530

    backend created

---
+++ Backend/server.js
@@ -0,0 +1,27 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const cors=require("cors")
+
+require("dotenv").config()
+
+const app=express()
+
+app.use(cors());
+app.use(express.json())
+
+app.get("/",(req,res)=>{
+    res.send("server is running.....")
+})
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected ✅"))
+  .catch((err) => console.log(err));
+
+
+
+  
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
